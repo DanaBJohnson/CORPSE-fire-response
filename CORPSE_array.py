ef97bfad16f8b34fb8d4589991786349c65bf162
DanaBJohnson <dbjohnson7@wisc.edu>
2024-12-20 11:11:49 -0500

    Updating scripts to run multi-MBC pool model

--- CORPSE_array.py
+++ CORPSE_array.py
@@ -1,31 +1,36 @@
 # This file holds the definition and key functions of the actual CORPSE model
 
 # List of parameters the model needs
-expected_params={	'vmaxref': 'Relative maximum enzymatic decomp rates (length 3)',
+expected_params={'vmaxref': 'Relative maximum enzymatic decomp rates for each microbe pool (length 3)',
         	'Ea':	'Activation energy (length 3)',
         	'kC':	'Michaelis-Menton parameter (length 3)',
         	'gas_diffusion_exp': 'Determines suppression of decomp at high soil moisture',
             'substrate_diffusion_exp':'Determines suppression of decomp at low soil moisture',
-        	'minMicrobeC':	   'Minimum microbial biomass (fraction of total C)',
-        	'Tmic': 'Microbial lifetime at 20C (years)',
-        	'et':		  'Fraction of turnover not converted to CO2',
+        	'minMicrobeC':	   'Minimum microbe biomass (fraction of total C)',
+        	'Tmic': 'microbe lifetime at 20C (years)',
+        	'et':  'Fraction of turnover from first microbial pool not converted to CO2',
         	'eup': 'Carbon uptake efficiency (length 3)',
         	'tProtected':	'Protected C turnover time (years)',
         	'protection_rate':'Protected carbon formation rate (year-1) (length 3)',
             'new_resp_units':True,
             }
 
+
 # Names of the C types. Can edit this to change the number and name of pools in edited model simulations
 chem_types = ['Fast','Slow','Necro', 'Py']
+
+# set number of microbial pools (up to 4) to use - would be nice to integrate this into Microbial_sims.py
+microbial_pools = ['MBC_1','MBC_2','MBC_3','MBC_4'] 
 
 # Makes a list of the pools that should actually be in the model, including both protected and unprotected states
 expected_pools = ['u'+t+'C' for t in chem_types]+\
                  ['p'+t+'C' for t in chem_types]+\
-                 ['livingMicrobeC','CO2','originalC']
+                 [m for m in microbial_pools]+\
+                 ['CO2','originalC']
 
 # Function for updating protected C formation rate for different soil textures, based on Mayes et al (2012) paper
-#All soils: slope=0.4833,intercept=2.3282
-#Alfisols: slope=0.5945, intercept=2.2788
+# All soils: slope=0.4833,intercept=2.3282
+# Alfisols: slope=0.5945, intercept=2.2788
 def prot_clay(claypercent,slope=0.4833,intercept=2.3282,BD=1.15,porosity=0.4):
     ''' Calculate protection rate as a function of clay content, based on sorption isotherms from Mayes et al (2012) Table 3
     Calculates Qmax in mgC/kg soil from Mayes et al 2012, converted to g/m3 using bulk density
@@ -46,10 +51,10 @@ def check_params(params):
              Ea=[37e3,54e3,50e3];     Activation energy
              kC=[0.01,0.01,0.01];     Michaelis-Menton parameter
              gas_diffusion_exp=2.5;   Determines suppression of decomp at high soil moisture
-             minMicrobeC=1e-3;       Minimum microbial biomass (fraction of total C)
-             Tmic=0.15;        Microbial turnover rate
+             minMicrobeC=1e-3;       Minimum microbe biomass (fraction of total C)
+             Tmic=0.15;        microbe turnover rate
              et=0.5;           Fraction of turnover not converted to CO2
-             eup=[0.6,0.05,0.6];  Carbon uptake efficiency
+             eup=[0.6,0.05,0.6];  Carbon uptake efficiency - slow growing microbes
              tProtected=75.0;     Protected C turnover time (years)
              protection_rate=[1.0,0.0,1.0];  Protected carbon formation rate (year-1)'''
 
@@ -75,52 +80,77 @@ def CORPSE_deriv(SOM,T,theta,params,claymod=1.0):
 
        Returns same data structure as SOM'''
 
+    # convert input into array
     theta=atleast_1d(theta)
     T=atleast_1d(T)
 
+    # Constraint theta to 0 < theta < 1
     theta[theta<0]=0.0
     theta[theta>1]=1.0
 
-    et=params['et']
-    eup=params['eup']
-
-    # Calculate maximum potential C decomposition rate
+    # Calculate maximum potential C decomposition rate of each C pool by each microbial group
     decomp=decompRate(SOM,T,theta,params)
 
-    # Microbial turnover
-    microbeTurnover=(SOM['livingMicrobeC']-params['minMicrobeC']*(sumCtypes(SOM,'u')))/params['Tmic'];   # kg/m2/yr
-    if isinstance(microbeTurnover,float):
-        microbeTurnover=max(0.0,microbeTurnover)
-    else:
-        microbeTurnover[microbeTurnover<0.0]=0.0
+    # Create empty variables for dead MBC and microbial turnover rate
+    deadmic_C_production=0
+    microbeTurnover={}
+    maintenanceResp={}
+    total_maintenanceResp=0
+    CO2prod=0
+    microbeGrowth={}
+    
+    # Calculate microbial turnover and dead MBC production for each microbial pool. If MBC pool > 0, calculate microbial turnover. If not, set turnover equal to 0.
+    microbeTurnover=dict([(m, where((SOM[m]>0), (SOM[m]-params['minMicrobeC'][m]*(sumCtypes(SOM,'u')))/params['Tmic'][m],0)) for m in microbial_pools])
+    
+    for m in microbial_pools:    
+        # Ensure that microbial turnover is > 0
+        if isinstance(microbeTurnover[m],float):
+            microbeTurnover[m]=max(0.0,microbeTurnover[m])
+        else:
+            microbeTurnover[m][microbeTurnover[m]<0.0]=0.0
+            
+        deadmic_C_production=deadmic_C_production+microbeTurnover[m]*params['et'][m]
+        maintenanceResp[m]=microbeTurnover[m]*(1.0-params['et'][m])
+        total_maintenanceResp=total_maintenanceResp+maintenanceResp[m]
+        CO2prod=CO2prod+maintenanceResp[m]
+    
+        microbeGrowth[m]=0
+        for t in chem_types: 
+            CO2prod=CO2prod + decomp[m][t]*(1.0-params['eup'][m][t])
+            microbeGrowth[m]=microbeGrowth[m]+decomp[m][t]*params['eup'][m][t]
 
-    maintenance_resp=microbeTurnover*(1.0-et)
 
-    deadmic_C_production=microbeTurnover*et   # actual fraction of microbial turnover
 
-    # CO2 production and cumulative CO2 produced by cohort
-    CO2prod=maintenance_resp
-    for t in chem_types:
-        CO2prod=CO2prod+decomp[t]*(1.0-eup[t])
-
-    microbeGrowth=CO2prod*0.0
-    for t in chem_types:
-        microbeGrowth=microbeGrowth+decomp[t]*eup[t]
 
     # Update protected carbon
     protectedCturnover = dict([(t,SOM['p'+t+'C']/params['tProtected']) for t in chem_types])
     protectedCprod =     dict([(t,SOM['u'+t+'C']*params['protection_rate'][t]*claymod) for t in chem_types])
-
-    derivs=SOM.copy()
+        # Some slow, protected C is being formed, but none of the pools are undergoing turnover > 0. Where is the protected C coming from??
+        
+        
+    #### Assign new values to SOM pools (based on calculated rates of change)
+    derivs=SOM.copy() # Create a copy of the original SOM dictionary
     for k in derivs.keys():
-        derivs[k]=0.0
-    derivs['livingMicrobeC']=microbeGrowth-microbeTurnover
+        derivs[k]=0.0 # Set all the C pools to 0
+        
+    # Fill in dictionary with new MBC pool size based on microbial growth and turnover 
+    for m in microbial_pools:
+        derivs[m]=microbeGrowth[m]-microbeTurnover[m]
+
     derivs['CO2']=CO2prod
 
+    total_decomp={}   # create empty dictionary 
     for t in chem_types:
-        derivs['u'+t+'C']=-decomp[t]+protectedCturnover[t]-protectedCprod[t]
-        derivs['p'+t+'C']=protectedCprod[t]-protectedCturnover[t]
+        total_decomp['u'+t+'C']=0 # Assign an initial value of 0 to for decomposition rate of C pool
+        total_decomp['p'+t+'C']=0
+        for m in microbial_pools:
+            # Calculate cumulative decomposition of each pool by each microbial group
+            total_decomp['u'+t+'C']=total_decomp['u'+t+'C']+decomp[m][t]
+        # Fill in derivs dictionary with new C pool (Fast, Slow, Necro, Py) size based on decomposition rate, protected C formation, and protected C turnover
+        derivs['u'+t+'C']=-total_decomp['u'+t+'C']+protectedCturnover[t]-protectedCprod[t]
+        derivs['p'+t+'C']=-total_decomp['p'+t+'C']+protectedCprod[t]-protectedCturnover[t]
 
+    # Add new dead MBC to the necromass pool 
     derivs['uNecroC']=derivs['uNecroC']+deadmic_C_production
     return derivs
 
@@ -138,21 +168,22 @@ def decompRate(SOM,T,theta,params):
 
     else:
         aerobic_max=1.0
+ 
+    vmax=dict([(m, Vmax(m,T,params)) for m in microbial_pools])
+        
+    # Skip the decomposition calculation if there is no carbon or no microbe biomass (to avoid dividing by zero)
+    dodecomp=dict([(m,(sumCtypes(SOM,'u')!=0.0)&(theta!=0.0)&(SOM[m]!=0.0)) for m in microbial_pools])
 
-    vmax=Vmax(T,params)
-
-    # Decomposition rate of each C type
+    # Create empty dictionary to store decomposition rates
     decompRate={}
-    # Skip the decomposition calculation if there is no carbon or no microbial biomass (to avoid dividing by zero)
-    dodecomp=(sumCtypes(SOM,'u')!=0.0)&(theta!=0.0)&(SOM['livingMicrobeC']!=0.0)
-    for t in chem_types:
-        if dodecomp.any():
-            drate=where(dodecomp,vmax[t]*theta**params['substrate_diffusion_exp']*(SOM['u'+t+'C'])*SOM['livingMicrobeC']/(sumCtypes(SOM,'u')*params['kC'][t]+SOM['livingMicrobeC'])*(1.0-theta)**params['gas_diffusion_exp']/aerobic_max,0.0)
-        decompRate[t]=drate
+    
+    for m in microbial_pools:
+        decompRate[m]=dict([(t, where(dodecomp[m],vmax[m][t]*theta**params['substrate_diffusion_exp']*(SOM['u'+t+'C'])*SOM[m]/(sumCtypes(SOM,'u')*params['kC'][m][t]+(SOM['MBC_1']+SOM['MBC_2']+SOM['MBC_3']+SOM['MBC_4']))*(1.0-theta)**params['gas_diffusion_exp']/aerobic_max,0.0)) for t in chem_types])
 
-    return decompRate
+    return decompRate # output is the decomposition rates of each C pool by given MBC group
 
-def Vmax(T,params):
+
+def Vmax(Micro_pool, T,params):
     '''Vmax function, normalized to Tref=293.15
     T is in K'''
 
@@ -161,11 +192,13 @@ def Vmax(T,params):
 
     from numpy import exp
 
-    Vmax=dict([(t,params['vmaxref'][t]*exp(-params['Ea'][t]*(1.0/(Rugas*T)-1.0/(Rugas*Tref)))) for t in chem_types]);
+    # Calculate temperature adjusted vmax for each chem type
+    Vmax=dict([(t,params['vmaxref'][Micro_pool][t]*exp(-params['Ea'][t]*(1.0/(Rugas*T)-1.0/(Rugas*Tref)))) for t in chem_types]);
+
     return Vmax
 
 # Add together the C types. prefix is for specifying protected or unprotected (p or u)
-#  Doesn't include living MBC
+#  Doesn't include living MBC or CO2
 def sumCtypes(SOM,prefix):
     out=SOM[prefix+chem_types[0]+'C']
     if len(chem_types)>1:
--- Whitman_sims.py
+++ Whitman_sims.py
@@ -8,27 +8,40 @@ import copy
 # There are three kinds of chemically-defined C (Fast, slow, and microbial necromass). "Fast" has higher maximum decomposition rate and microbial CUE
 # Each C type can be in a protected or unprotected state. When protected, it is not subject to microbial decomposition
 SOM_init={'CO2': array(0.0),    # Cumulative C-CO2 from microbial respiration
- 'livingMicrobeC': array(5.0), # Active, living microbial biomass
- 'pFastC': array(0.0),         # Protected fast-decomposing C
- 'pNecroC': array(0.0),        # Protected microbial necromass C
- 'pSlowC': array(0.0),         # Protected slow-decomposing C
+ 'MBC_1': array(4.5),           # Active, living microbial biomass
+ 'MBC_2': array(0.045),         # MBC_2 = 1% of MBC_1
+ 'MBC_3': array(0.00),
+ 'MBC_4': array(0.00), 
+ 'pFastC': array(0.0),          # Protected fast-decomposing C
+ 'pNecroC': array(0.0),         # Protected microbial necromass C
+ 'pSlowC': array(0.0),          # Protected slow-decomposing C
  'pPyC': array(0.0),            # Protected PyC
  'uFastC': array(3.0),          # Unprotected fast-decomposing C, Changed value from 10 to 11 based on calibration results
- 'uNecroC': array(3.0),        # Unprotected microbial necromass C, 
- 'uSlowC': array(88),         # Unprotected slow-decomposing C, Changed value from 86 to 60.2 based on calibration results
+ 'uNecroC': array(3.0),         # Unprotected microbial necromass C, 
+ 'uSlowC': array(88),           # Unprotected slow-decomposing C, Changed value from 86 to 60.2 based on calibration results
  'uPyC': array(4.0)}            # Unprotected PyC
+
  
 # Parameters controlling the model (sandy soil, no burn)
 params={
-    'vmaxref':{'Fast':8.0,'Slow':0.2,'Necro':4.0, 'Py':0.1}, #  Relative maximum enzymatic decomp rates for each C type (year-1)
+    'vmaxref':{'MBC_1':{'Fast':6.9,'Slow':0.11,'Necro':7.0, 'Py':0.1},      #  Relative maximum enzymatic decomp rates for each C type (year-1)
+               'MBC_2':{'Fast':19.2,'Slow':0.0064,'Necro':45.0, 'Py':0.01},
+               'MBC_3':{'Fast':0.0,'Slow':0.0,'Necro':0.0, 'Py':0.0}, 
+               'MBC_4':{'Fast':0.0,'Slow':0.0,'Necro':0.0, 'Py':0.0}},
     'Ea':{'Fast':5e3,'Slow':30e3,'Necro':5e3, 'Py':35e3},      # Activation energy (controls T dependence)
-    'kC':{'Fast':0.01,'Slow':0.01,'Necro':0.01, 'Py':0.01},    # Michaelis-Menton half saturation parameter (g microbial biomass/g substrate)
+    'kC':{'MBC_1':{'Fast':0.01,'Slow':0.01,'Necro':0.01, 'Py':0.01},    # Michaelis-Menton half saturation parameter (g microbial biomass/g substrate)
+          'MBC_2':{'Fast':0.01,'Slow':0.04,'Necro':0.01, 'Py':0.04},    # Michaelis-Menton half saturation parameter (g microbial biomass/g substrate)
+          'MBC_3':{'Fast':0.1,'Slow':0.01,'Necro':0.1, 'Py':0.01},    # Michaelis-Menton half saturation parameter (g microbial biomass/g substrate)
+          'MBC_4':{'Fast':0.1,'Slow':0.01,'Necro':0.1, 'Py':0.01}},    # Michaelis-Menton half saturation parameter (g microbial biomass/g substrate)
     'gas_diffusion_exp':0.6,  # Determines suppression of decomposition at high soil moisture
     'substrate_diffusion_exp':1.5,   # Controls suppression of decomp at low soil moisture
-    'minMicrobeC':1e-3,       # Minimum microbial biomass (fraction of total C). Prevents microbes from going extinct and allows some slow decomposition under adverse conditions
-    'Tmic':0.25,              # Microbial biomass mean lifetime (years)
-    'et':0.6,                 # Fraction of microbial biomass turnover (death) that goes to necromass instead of being immediately mineralized to CO2
-    'eup':{'Fast':0.8,'Slow':0.4,'Necro':0.8, 'Py':0.1},     # Microbial carbon use efficiency for each substrate type (fast, slow, necromass). This amount is converted to biomass during decomposition, and the remainder is immediately respired as CO2
+    'minMicrobeC':{'MBC_1':1e-3,'MBC_2':1e-5,'MBC_3':1e-3,'MBC_4':1e-3}, # Minimum microbial biomass (fraction of total C). Prevents microbes from going extinct and allows some slow decomposition under adverse conditions
+    'Tmic':{'MBC_1':0.5,'MBC_2':0.15,'MBC_3':0.25,'MBC_4':0.25}, # Microbial biomass mean lifetime (years)
+    'et':{'MBC_1':0.8,'MBC_2':0.8,'MBC_3':0.6,'MBC_4':0.6},                  # Fraction of microbial biomass turnover (death) that goes to necromass instead of being immediately mineralized to CO2
+    'eup':{'MBC_1':{'Fast':0.6,'Slow':0.3,'Necro':0.65, 'Py':0.15},      # Microbial carbon use efficiency for each substrate type (fast, slow, necromass). This amount is converted to biomass during decomposition, and the remainder is immediately respired as CO2
+            'MBC_2':{'Fast':0.36,'Slow':0.1,'Necro':0.3, 'Py':0.05},     
+            'MBC_3':{'Fast':0.5,'Slow':0.3,'Necro':0.6, 'Py':0.1},
+            'MBC_4':{'Fast':0.5,'Slow':0.3,'Necro':0.6, 'Py':0.1}},
     'tProtected':75.0,        # Protected C turnover time (years). This is the time scale for which protected C is released back to unprotected state.
     'protection_rate':{'Fast':0.0,'Slow':0.001,'Necro':0, 'Py':0}, # Protected carbon formation rate (year-1). Higher number means more will become protected. Can be modified as a function of soil texture/mineralogy to represent different sorption potentials
     'new_resp_units':True,   # At some point I changed the units of vmaxref to be normalized for other factors so they are actually in year-1 units. Leave this as True values that are easier to interpret.
@@ -58,9 +71,12 @@ initvals['no burn sandy soil']=copy.deepcopy(SOM_init)      # Makes a copy of th
 paramsets['no burn sandy soil']=copy.deepcopy(params)
 envir_params['no burn sandy soil']=copy.deepcopy(envir_vals)
 
-# Sandy soil, high severity burn
+# Simulation: Incubation of sandy soil following high severity burn
 initvals['high sev burn sandy soil']=copy.deepcopy(SOM_init)       # Makes a copy of the default initial values. Need to use deepcopy so changing the value here doesn't change it for every simulation
-initvals['high sev burn sandy soil']['livingMicrobeC']=array(0.6) # Start with low initial microbial biomass. Assumes this simulation starts right after the fire
+initvals['high sev burn sandy soil']['MBC_1']=array(1) # Start with low initial microbial biomass. Assumes this simulation starts right after the fire
+initvals['high sev burn sandy soil']['MBC_2']=array(0.3)
+initvals['high sev burn sandy soil']['MBC_3']=array(0.0)
+initvals['high sev burn sandy soil']['MBC_4']=array(0.0)
 initvals['high sev burn sandy soil']['uFastC']=array(0.75) 
 initvals['high sev burn sandy soil']['uNecroC']=array(0.5) 
 initvals['high sev burn sandy soil']['uSlowC']=array(80) # Changed value from 80 to 100 based on calibration results
@@ -79,50 +95,6 @@ envir_params['high sev burn sandy soil']['thetamin']=array(0.5)
 envir_params['high sev burn sandy soil']['thetamax']=array(0.7)
 envir_params['high sev burn sandy soil']['porosity']=array(0.4)
 
-# Org. soil, no burn
-initvals['no burn org soil']=copy.deepcopy(SOM_init)       # Makes a copy of the default initial values. Need to use deepcopy so changing the value here doesn't change it for every simulation
-initvals['no burn org soil']['livingMicrobeC']=array(6.0) # Start with low initial microbial biomass. Assumes this simulation starts right after the fire
-initvals['no burn org soil']['uFastC']=array(6.0) 
-initvals['no burn org soil']['uNecroC']=array(1.2) 
-initvals['no burn org soil']['uSlowC']=array(72) # Changed value from 86.0 to 60 based on calibration results
-initvals['no burn org soil']['uPyC']=array(5.0)
-paramsets['no burn org soil']=copy.deepcopy(params)
-paramsets['no burn org soil']['vmaxref']['Fast']=7.25     # 
-paramsets['no burn org soil']['vmaxref']['Slow']=0.1
-paramsets['no burn org soil']['vmaxref']['Necro']=7.25
-paramsets['no burn org soil']['vmaxref']['Py']=0.1
-paramsets['no burn org soil']['eup']['Fast'] = 0.9        # 
-paramsets['no burn org soil']['eup']['Slow'] = 0.4
-paramsets['no burn org soil']['eup']['Necro'] = 0.7
-paramsets['no burn org soil']['eup']['Py'] = 0.1
-envir_params['no burn org soil']=copy.deepcopy(envir_params)
-envir_params['no burn org soil']['thetamin']=array(0.5)
-envir_params['no burn org soil']['thetamax']=array(0.7)
-envir_params['no burn org soil']['porosity']=array(0.9)
-
-
-# Org soil, high severity burn
-initvals['high sev burn org soil']=copy.deepcopy(SOM_init)       # Makes a copy of the default initial values. Need to use deepcopy so changing the value here doesn't change it for every simulation
-initvals['high sev burn org soil']['livingMicrobeC']=array(0.6) # Start with low initial microbial biomass. Assumes this simulation starts right after the fire
-initvals['high sev burn org soil']['uFastC']=array(0.6) 
-initvals['high sev burn org soil']['uNecroC']=array(0.6) 
-initvals['high sev burn org soil']['uSlowC']=array(86) # Changed value from 86 to 60 based on calibration results
-initvals['high sev burn org soil']['uPyC']=array(12)
-paramsets['high sev burn org soil']=copy.deepcopy(params)
-paramsets['high sev burn org soil']['vmaxref']['Fast']=75.0     # 
-paramsets['high sev burn org soil']['vmaxref']['Slow']=0.15
-paramsets['high sev burn org soil']['vmaxref']['Necro']=75.0
-paramsets['high sev burn org soil']['vmaxref']['Py']=0.1
-paramsets['high sev burn org soil']['eup']['Fast'] = 0.4        # 
-paramsets['high sev burn org soil']['eup']['Slow'] = 0.3
-paramsets['high sev burn org soil']['eup']['Necro'] = 0.4
-paramsets['high sev burn org soil']['eup']['Py'] = 0.1
-envir_params['high sev burn org soil']=copy.deepcopy(envir_params)
-envir_params['high sev burn org soil']['thetamin']=array(0.5)
-envir_params['high sev burn org soil']['thetamax']=array(0.7)
-envir_params['high sev burn org soil']['porosity']=array(0.9)
-
-
 
 # Set up a data structure to hold the results of the different simulations
 results={}
@@ -130,54 +102,81 @@ results={}
 # Simulations are assuming a constant temperature of 20 C and constant moisture of 60% of saturation
 # Inputs are empty because this is running as an incubation without any constant inputs of C
 
-
-
 for functype in initvals:
     results[functype] = CORPSE_solvers.run_models_ODE(Tmin=18.0,Tmax=24.0,thetamin=envir_params[functype]['thetamin'],
                                                       thetamax=envir_params[functype]['thetamax'],
                                             times=t,inputs={},clay=2.5,initvals=initvals[functype],params=paramsets[functype])
 
 
+# Tally total number of microbial pools being used in simulation
+from numpy import where
+num_micro_pools=0
+num_micro_pools=where(SOM_init['MBC_1']>0, num_micro_pools+1, num_micro_pools+0)   
+num_micro_pools=where(SOM_init['MBC_2']>0, num_micro_pools+1, num_micro_pools+0)   
+num_micro_pools=where(SOM_init['MBC_3']>0, num_micro_pools+1, num_micro_pools+0)   
+num_micro_pools=where(SOM_init['MBC_4']>0, num_micro_pools+1, num_micro_pools+0)   
 
 
-## microbial pools:
-# # Resistant: Low biomass loss, fast growth
-# # Fast-growing survivor
-# initvals['Fast-growing survivor']=copy.deepcopy(SOM_init)      # Makes a copy of the default initial values. Need to use deepcopy so changing the value here doesn't change it for every simulation
-# initvals['Fast-growing survivor']['livingMicrobeC']=array(0.01) # Start with a high amount of initial microbial biomass. Assumes this simulation starts right after the fire
-# paramsets['Fast-growing survivor']=copy.deepcopy(params)
-# paramsets['Fast-growing survivor']['vmaxref']['Fast']=20.0     # Higher potential decomposition rate for more labile C
-# paramsets['Fast-growing survivor']['eup']['Fast'] = 0.2
-# paramsets['Fast-growing survivor']['eup']['Necro'] = 0.2
+# This section plots the results
+# Each set of results should have the same set of pools as the initial values structure from the beginning of the simulation
+from matplotlib import pyplot
+
+# Plot CO2 fluxes
+fig,ax=pyplot.subplots(nrows=1,ncols=1,clear=True,num='CORPSE results')
+
+for sim in results:
+    totalC=CORPSE_array.sumCtypes(results[sim][0], 'u')+CORPSE_array.sumCtypes(results[sim][0], 'p')
+    ax.plot(t*365,results[sim][0]['CO2'].diff()/totalC[0]*100,label=sim)
+    # ax[1].plot(t*365,results[sim][0]['uFastC'],label='Simple')
+    # ax[1].plot(t*365,results[sim][0]['uSlowC'],label='Complex')
+    # ax[1].plot(t*365,results[sim][0]['uNecroC'],label='Necromass')
+ax.set_xlabel('Time (days)')
+ax.set_ylabel('CO$_2$ flux rate (% initial C/day)')
+ax.legend(fontsize='small')
+ax.set_title('CO$_2$ fluxes')
+
+pyplot.show()
+    
 
 
-# # Susceptible: High biomass loss, slow growth
-# # Fire susceptible
-# initvals['Fire susceptible']=copy.deepcopy(SOM_init)
-# initvals['Fire susceptible']['livingMicrobeC']=array(0.001)    # Low initial microbial biomass
-# paramsets['Fire susceptible']=copy.deepcopy(params)
-# paramsets['Fire susceptible']['vmaxref']['Fast']=0.2          # Slower maximum decomposition rate for labile C
-# paramsets['Fire susceptible']['eup']['Fast'] = 0.5
-# paramsets['Fire susceptible']['eup']['Necro'] = 0.5
+# Plot microbial pool sizes
+nrows=int(num_micro_pools)
 
+fig,ax=pyplot.subplots(nrows=nrows,ncols=1,clear=True,num='CORPSE results')
+for sim in results:
+    if nrows == 1: 
+        ax.plot(t*365,results[sim][0]['MBC_1']/totalC[0]*100)
+        ax.set_ylabel('MBC 1')
+        ax.set_xlabel('Time (days)')
+        ax.legend(fontsize='small')
+        ax.set_title('Microbial biomass C pool size (% of initial C)')
+    elif nrows == 2: 
+        ax[0].plot(t*365,results[sim][0]['MBC_1']/totalC[0]*100)
+        ax[0].set_ylabel('MBC 1')
+        ax[1].plot(t*365,results[sim][0]['MBC_2']/totalC[0]*100)  
+        ax[1].set_ylabel('MBC 2')
+        ax[1].set_xlabel('Time (days)')
+    elif nrows ==3: 
+        ax[0].plot(t*365,results[sim][0]['MBC_1']/totalC[0]*100)
+        ax[0].set_ylabel('MBC 1')
+        ax[1].plot(t*365,results[sim][0]['MBC_2']/totalC[0]*100)  
+        ax[1].set_ylabel('MBC 2')
+        ax[2].plot(t*365,results[sim][0]['MBC_3']/totalC[0]*100)
+        ax[2].set_ylabel('MBC 3')
+        ax[2].set_xlabel('Time (days)')
+    elif nrows == 4:
+        ax[0].plot(t*365,results[sim][0]['MBC_1']/totalC[0]*100)
+        ax[0].set_ylabel('MBC 1')
+        ax[1].plot(t*365,results[sim][0]['MBC_2']/totalC[0]*100)  
+        ax[1].set_ylabel('MBC 2')
+        ax[2].plot(t*365,results[sim][0]['MBC_3']/totalC[0]*100)
+        ax[2].set_ylabel('MBC 3')
+        ax[3].plot(t*365,results[sim][0]['MBC_4']/totalC[0]*100)
+        ax[3].set_ylabel('MBC 4')
+        ax[3].set_xlabel('Time (days)')
 
-# # Recovering: High biomass loss, fast growth
-# # Post-fire rebounder
-# initvals['Post-fire rebounder']=copy.deepcopy(SOM_init)
-# initvals['Post-fire rebounder']['livingMicrobeC']=array(0.001) # Low initial microbial biomass
-# paramsets['Post-fire rebounder']=copy.deepcopy(params)
-# paramsets['Post-fire rebounder']['vmaxref']['Fast']=20.0      # Very fast potential decomposition/growth rate
-# paramsets['Post-fire rebounder']['eup']['Fast'] = 0.2
-# paramsets['Post-fire rebounder']['eup']['Necro'] = 0.2
+ax[0].legend(fontsize='small')
+ax[0].set_title('Microbial biomass C pool size (% of initial C)')
+# ax[1].set_title('Microbial biomass')
 
-
-# # Resilient: Low biomass loss, slow growth
-# # Slow-growing survivor
-# initvals['Slow-growing survivor']=copy.deepcopy(SOM_init)
-# initvals['Slow-growing survivor']['livingMicrobeC']=array(0.01) # High initial microbial biomass
-# paramsets['Slow-growing survivor']=copy.deepcopy(params)
-# paramsets['Slow-growing survivor']['vmaxref']['Fast']=0.2      # Slower decomposition/growth rate
-# paramsets['Slow-growing survivor']['eup']['Fast'] = 0.5
-# paramsets['Slow-growing survivor']['eup']['Necro'] = 0.5
-
-
+pyplot.show()
